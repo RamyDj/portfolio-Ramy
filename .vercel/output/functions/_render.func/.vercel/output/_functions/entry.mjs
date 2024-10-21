866b9b0b079ec1835e0318ba1112e86968cb2d3e
Ramy <ramyto.rd@gmail.com>
2024-10-21 15:20:14 +0200

    maj

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_DlFbV1wr.mjs';
-import { manifest } from './manifest_QtIR7Ej1.mjs';
+import { manifest } from './manifest_CM7DJdGs.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/index.astro.mjs');
@@ -17,7 +17,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "5023a68e-5334-476c-8d63-8a38cfb116ba",
+    "middlewareSecret": "6ea31d05-f8cf-4b92-b0a5-edbb4101718c",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
